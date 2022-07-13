86c560727ad5055e52a5e8027938323061be4b4d
RQu <balanar99@outlook.com>
2022-07-13 11:17:46 +0200

    fetchTMDB

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,9 +1,15 @@
-const Main = () => {
+import Thumbnail from "./Thumbnail/Thumbnail";
+
+const Main = ({ results }) => {
   return (
     <main>
-
+      <ul>
+        {results.map((result) => (
+          <Thumbnail key={result.id} result={result} />
+        ))}
+      </ul>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['i.pinimg.com','press.hulu.com']
+    domains: ['i.pinimg.com','press.hulu.com',`image.tmdb.org`]
  }
 }
 
